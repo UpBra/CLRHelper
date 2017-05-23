227eff2d096582135c9b585429762ea956f0a770
Blair <blair.replogle@possible.com>
2017-05-23 15:27:44 -0600

    Initial Commit

---
+++ CLRHelper/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CLRHelper
+//
+//  Created by Blair Replogle on 5/23/17.
+//  Copyright © 2017 Gleesh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
